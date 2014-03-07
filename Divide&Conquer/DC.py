5f52447a4e8b4c07af0781cb39f2ee015b5f2d24
Christos Louizos <chr.louizos@gmail.com>
2014-03-07 10:43:19 +0100

    More reasonable parameters.

--- Divide&Conquer/DC.py
+++ Divide&Conquer/DC.py
@@ -7,7 +7,7 @@ import load_data as ld
 data_total = ld.load_pickle('','data_cluster.pickle')
 print data_total.shape
 # fit the data with DPGMM
-dpgmm = DPGMM(covariance_type='tied', alpha=1000000., n_iter=1000, n_components=30)
+dpgmm = DPGMM(covariance_type='diag', alpha=2., n_iter=1000, n_components=30)
 dpgmm.fit(data_total)
 print dpgmm.predict(data_total)
 print 'aic criterion: %f' %dpgmm.aic(data_total)
