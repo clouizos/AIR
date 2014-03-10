4917a784a4b4142bbefc7eab26b4d06bfddbb018
anoukv <anouk.visser@student.uva.nl>
2014-03-10 10:06:58 +0100

    started extended userLM

--- documentReranking/similarityMeasures/userSimilarities.py
+++ documentReranking/similarityMeasures/userSimilarities.py
@@ -1,5 +1,6 @@
 # I'm feeling it's not so good to rely on the termfrequencies in the user class...
 import userLM as user
+import userLM as userExtended
 import pickle
 
 print "Importing term frequences..."
@@ -7,7 +8,6 @@ termFrequencies = pickle.load(open('../../../termFrequencies_strict', 'rb'))
 
 print "Importing user queries..."
 userQueries = pickle.load(open('../../../userQueries_strict', 'rb'))
-
 
 def sim(userA, userB, minCommonTerms):
 	# create two user objects
@@ -45,13 +45,46 @@ def mutualSim(userA, userB, minCommonTerms):
 		for query in B.queries:
 			similarityScoreBA += A.p_q_u(query)
 		similarityScoreBA = similarityScoreBA / B.numberOfQueries
+	
 	else:
 		similarityScoreAB = -9999
+		similarityScoreBA = -9999
 
 	return similarityScoreAB + similarityScoreBA
 
+def simExtended(userA, userB, minCommonTerms):
+	A = userExtended.UserLMExtended(userA)
+	B = userExtended.UserLMExtended(userB)
 
-def hoi():
+	print A.clickedDocuments
+	print B.clickedDocuments
+
+
+def test1():
+	print "Found ", len(userQueries.keys()), " users"
+	print len(termFrequencies.keys()), " users have queries"
+
+	for userA in termFrequencies.keys():
+		bestMatch = -9999
+		bestUser = 0
+		for userB in termFrequencies.keys():
+			if userB != userA:
+				similarity = sim(userA, userB, 5)
+				if similarity > bestMatch:
+					bestMatch = similarity
+					bestUser = userB
+		if bestUser != 0:
+			print
+			print "Best matching score: ", bestMatch
+			print "Queries user A: ", user.UserLM(userA).queries
+			print "Queries user B: ", user.UserLM(bestUser).queries
+			print "#queries A: ", len(user.UserLM(userA).queries)
+			print "#queries B: ", len(user.UserLM(bestUser).queries)
+		else:
+			print 
+			print "No best match!"
+
+def test2():
 	print "Found ", len(userQueries.keys()), " users"
 	print len(termFrequencies.keys()), " users have queries"
 
@@ -78,4 +111,4 @@ def hoi():
 
 
 
-hoi()
+test1()
