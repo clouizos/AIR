3137fcdd69ebfadb643de7ffa38ea734934e24f4
anoukv <anouk.visser@student.uva.nl>
2014-03-10 15:28:27 +0100

    mutual similarity extended

--- documentReranking/similarityMeasures/userSimilarities.py
+++ documentReranking/similarityMeasures/userSimilarities.py
@@ -70,6 +70,33 @@ def simExtended(userA, userB, minCommonTerms):
 
 	return  similarityScore
 
+def mutualSimExtended(userA, userB, minCommonTerms):
+	
+	A = userExtended.UserLMExtended(userA)
+	B = userExtended.UserLMExtended(userB)
+	
+	similarityScoreAB = 0
+	similarityScoreBA = 0
+
+	# check if they have minCommonTerms terms in common
+	# if not, return 0
+	if A.termsInCommon(B) > minCommonTerms:
+		# calculate the similarity A -> B
+		for query in A.queries:
+			# !! not very efficient since users are expected to have many duplicate queries... 
+			similarityScoreAB += B.p_q_u(query)
+		similarityScoreAB = similarityScoreAB / A.numberOfQueries
+
+		for query in B.queries:
+			similarityScoreBA += A.p_q_u(query)
+		similarityScoreBA = similarityScoreBA / B.numberOfQueries
+	
+	else:
+		similarityScoreAB = -9999
+		similarityScoreBA = -9999
+
+	return similarityScoreAB + similarityScoreBA
+
 
 def test1():
 	print "Testing sim"
@@ -133,6 +160,32 @@ def test3():
 		for userB in termFrequencies.keys():
 			if userB != userA:
 				similarity = simExtended(userA, userB, 5)
+				if similarity > bestMatch:
+					bestMatch = similarity
+					bestUser = userB
+		if bestUser != 0:
+			print
+			print "Best matching score: ", bestMatch
+			print "Queries user A: ", set(userExtended.UserLMExtended(userA).queries)
+			print "Queries user B: ", set(userExtended.UserLMExtended(bestUser).queries)
+			print "#queries A: ", len(userExtended.UserLMExtended(userA).queries)
+			print "#queries B: ", len(userExtended.UserLMExtended(bestUser).queries)
+		else:
+			print 
+			print "No best match!"
+
+def test4():
+
+	print "Testing simMutualExtended"
+	print "Found ", len(userQueries.keys()), " users"
+	print len(termFrequencies.keys()), " users have queries"
+
+	for userA in termFrequencies.keys():
+		bestMatch = -9999
+		bestUser = 0
+		for userB in termFrequencies.keys():
+			if userB != userA:
+				similarity = mutualSimExtended(userA, userB, 5)
 				if similarity > bestMatch:
 					bestMatch = similarity
 					bestUser = userB
