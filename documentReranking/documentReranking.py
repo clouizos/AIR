c00ec6c73629e0d3302ba7711f52b745aad53df3
anoukv <anouk.visser@student.uva.nl>
2014-03-11 10:05:32 +0100

    made re-ranking function

--- documentReranking/documentReranking.py
+++ documentReranking/documentReranking.py
@@ -7,12 +7,27 @@ numberOfSimilarUsers = 5
 minTermsInCommon = 5
 userID = 'UID48'
 # so we have a user
-u = user.User(userID)
+userA = user.User(userID)
+mostSimilarUsers = userA.getMostSimilarUsers(numberOfSimilarUsers, minTermsInCommon)
 
-for query in u.queries:
-	print query
-	print u.queryResults[query]
-print u.getMostSimilarUsers(numberOfSimilarUsers, minTermsInCommon)
+ranking = []
+for query in userA.queries:
+	documentsToReRank = userA.queryResults[query]
+	for doc in documentsToReRank:
+		score = getRankingScoreForDocument(mostSimilarUsers, doc)
+		ranking.append((doc, score))
+print ranking
+
+def getRankingScoreForDocument(similarUsers, document):
+	rank = 0
+	for uB in similarUsers:
+		userID = uB[0]
+		similarity = uB[1]
+		userB = user.User(userID)
+		if userB.didClickDocument(document):
+			rank += similarity
+	return rank
+
 
 
 
