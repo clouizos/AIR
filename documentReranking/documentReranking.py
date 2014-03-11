5473813e50be2c908755b21dc9f49d0f84b5a384
anoukv <anouk.visser@student.uva.nl>
2014-03-11 10:18:48 +0100

    just a small test after restructuring

--- documentReranking/documentReranking.py
+++ documentReranking/documentReranking.py
@@ -2,13 +2,8 @@
 # Created by Anouk Visser (27-02-2014)
 
 import user as user
+import pickle
 
-numberOfSimilarUsers = 25
-minTermsInCommon = 5
-userID = 'UID48'
-# so we have a user
-userA = user.User(userID)
-mostSimilarUsers = userA.getMostSimilarUsers(numberOfSimilarUsers, minTermsInCommon)
 
 
 def getRankingScoreForDocument(similarUsers, document):
@@ -23,15 +18,28 @@ def getRankingScoreForDocument(similarUsers, document):
 		rank = -9990
 	return rank
 
-for query in userA.queries:
-	documentsToReRank = userA.queryResults[query]
-	ranking = []
-	for doc in documentsToReRank:
-		score = getRankingScoreForDocument(mostSimilarUsers, doc)
-		ranking.append((doc, score))
-	print "Ranking for query: ", query
-	print ranking
-	print
+def createReRankingDump():
+	resultingRanks = dict()
+
+	numberOfSimilarUsers = 25
+	minTermsInCommon = 5
+	userID = 'UID48'
+	# so we have a user
+	userA = user.User(userID)
+	mostSimilarUsers = userA.getMostSimilarUsers(numberOfSimilarUsers, minTermsInCommon)
+
+	for query in userA.queries:
+		documentsToReRank = userA.queryResults[query]
+		ranking = []
+		for doc in documentsToReRank:
+			score = getRankingScoreForDocument(mostSimilarUsers, doc)
+			ranking.append((doc, score))
+		ranking.sort(key=lambda x : x[1], reverse=True)
+		toWriteToFile = [x[0] for elem in ranking]
+		print toWriteToFile
+
+createReRankingDump()
+
 
 
 
