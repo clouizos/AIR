c03e8e0017bf21828c8b21ee5a121e4bed665a67
anoukv <anouk.visser@student.uva.nl>
2014-03-12 10:28:57 +0100

    no documents?

--- documentReranking/document.py
+++ documentReranking/document.py
@@ -13,11 +13,10 @@ class Document:
 
 	def __init__(self, id):
 		if id in documentClicks:
+			print "FOUND IT!"
 			self.documentID = id
 			self.numberOFClicks = documentClicks[self.documentID]
 			self.queries = queriesLeadingToClick[self.documentID]
-		else:
-			print "Document missing: ", id
 
 
  
