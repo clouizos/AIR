6931e950517ab182c94ca43a9aad0402c64c63f9
anoukv <anouk.visser@student.uva.nl>
2014-03-13 10:05:51 +0100

    a slightly better speed test

--- documentReranking/user.py
+++ documentReranking/user.py
@@ -2,6 +2,7 @@
 # Created by Anouk Visser
 import similarityMeasures as sims
 import pickle
+import time
 
 allUsers = pickle.load(open('../../users_strict', 'rb'))['users']
 userQueriesAndClicks_strict = pickle.load(open('../../user_specific_positive_negative_examples_dic_strict', 'rb'))
@@ -10,8 +11,10 @@ class User:
 	userID = 0
 	queries = 0
 	queryResults = 0
+	clickedDocs = []
 	
 	def __init__(self, userID):
+		print "Creating new user object"
 		self.userID = userID
 
 		qs = set()
@@ -20,10 +23,12 @@ class User:
 		self.queries = qs
 
 		self.queryResults = dict( [ (x[0], x[1].union(x[2])) for x in userQueriesAndClicks_strict[userID] ])
+		self.clickedDocs = getClickedDocuments();
 
 	# returns 0/1 depending on whether the user clicked the document
 	def didClickDocument(self, document):
-		print "checking for click"
+		start_time = time.time()
+		print "Checking for click!"
 		click = 0
 		userInfo = userQueriesAndClicks_strict[self.userID]
 		for infoTriplet in userInfo:
@@ -31,8 +36,28 @@ class User:
 			if document in clickedDocs:
 				click = 1
 				break
-		print "done"
+
+		elapsed_time = time.time() - start_time
+		print "Method 1: ", elapsed_time, click
+
+		start_time = time.time()
+		if document in self.clickedDocs:
+			click = 1
+		elapsed_time = time.time() - start_time
+		print "Method 2: ", elapsed_time, click
+
+
+
 		return click
+
+	def getClickedDocuments(self):
+		clicks = set()
+		userInfo = userQueriesAndClicks_strict[self.id]
+		for infoTriplet in userInfo:
+			c = infoTriplet[1]
+			for d in c:
+				clicks.add(d)
+		return clicks
 	
 	# THIS SHOULD BE EXTENDED TO FIND MORE FORMS OF SIMILARITY
 	def getMostSimilarUsers(self, numberOfMostSimilarUsers, minTermsInCommon, whichModel, whichSim):
