650a573a5e49c0e914595cd024271c6aa18cd98f
RemideZ <remi.de.z@gmail.com>
2014-03-20 15:41:14 +0100

    nothing

--- documentReranking/documentReranking.py
+++ documentReranking/documentReranking.py
@@ -26,7 +26,7 @@ def getRankingScoreForDocument(similarUsers, document):
 		similarity = uB[1]
 		if userB.didClickDocument(document):
 			raw = exp(similarity)
-			rank *= raw * raw
+			rank *= sqrt(raw)
 	if rank == 0:
 		rank = -9990
 	return rank 
