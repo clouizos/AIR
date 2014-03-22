9f9e15155bbae7ed29f2ce1014e03e2030749112
anoukv <anouk.visser@student.uva.nl>
2014-03-22 13:19:08 +0100

    another print for debugging

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -178,6 +178,7 @@ def test():
 # Calculates precision at rank len(relevanceJudgements) (so the caller should provide the right k already)
 # input ranked list of document with indicator 1 for relevant, 0 for irrelevant example: [1, 1, 0, 0, 1, 0]
 def precisionAt(relevanceJudgements):
+	print sum(relevanceJudgements) / len(relevanceJudgements)
 	return sum(relevanceJudgements) / len(relevanceJudgements)
 
 # Calculates average precision of a ranked list
